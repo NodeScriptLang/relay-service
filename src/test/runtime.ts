804737fda2c0ea852ebc8540fca29384b0cce968
christellevs <christelle.marie@gmail.com>
2025-02-18 00:32:41 +0000

    test: add tests

--- src/test/runtime.ts
+++ src/test/runtime.ts
@@ -1,63 +1,35 @@
-import { AccountType, AuthTokenSchema, AuthTokenType, Permission } from '@nodescript/api-proto';
-import { JwtService } from '@nodescript/microframework';
 import { config } from 'dotenv';
 import { dep } from 'mesh-ioc';
 
 import { App } from '../main/app.js';
 import { HttpScope } from '../main/HttpScope.js';
-import { AuthContext } from '../main/scoped/AuthContext.js';
-import { NodeScriptApi } from '../main/scoped/NodeScriptApi.js';
-import { NodeScriptApiMock } from './mocks/NodeScriptApiMock.js';
-
+import { TestHttpServer } from './test-server.js';
 config({ path: '.env' });
 config({ path: '.env.test' });
 config({ path: '.env.dev' });
 
 export class TestRuntime {
 
-    @dep({ cache: false }) authContext!: AuthContext;
-    @dep({ cache: false }) jwt!: JwtService;
-    @dep({ cache: false }) nsApiMock!: NodeScriptApiMock;
+    @dep({ cache: false }) testHttpServer!: TestHttpServer;
 
     app = new App();
     httpScope = new HttpScope(this.app.mesh);
 
     async setup() {
         this.app = new App();
-        this.httpScope = new HttpScope(this.app.mesh);
-        // Allow resolving deps directly from the session scope
-        this.httpScope.connect(this);
-        this.httpScope.service(NodeScriptApiMock);
-        this.httpScope.alias(NodeScriptApi, NodeScriptApiMock);
+        this.app.mesh.connect(this);
+        this.app.mesh.service(TestHttpServer);
         await this.app.start();
+        await this.testHttpServer.start();
     }
 
     async teardown() {
+        await this.testHttpServer.stop();
         await this.app.stop();
     }
 
-    // async setupDatabases() {
-    //     await this.mongodb.start();
-    //     await this.mongodb.db.dropDatabase();
-    // }
-
-    // async stopDatabases() {
-    //     await this.mongodb.stop();
-    // }
-
-    authenticateWorkspace(orgId: string, workspaceId: string, scopes: Permission[]) {
-        const authToken = AuthTokenSchema.create({
-            tokenType: AuthTokenType.ACCESS_TOKEN,
-            principal: {
-                id: 'test',
-                type: AccountType.SYSTEM,
-                displayName: 'Test',
-            },
-            orgId,
-            workspaceId,
-            scopes,
-        });
-        this.authContext.set(authToken);
+    get baseUrl() {
+        return `http://localhost:${process.env.HTTP_PORT ?? '8080'}`;
     }
 
 }
